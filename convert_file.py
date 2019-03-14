f1fb01e2dcc6ba91136aacd1af7d90a3b835d95a
manliu1225 <liumanlalala@gmail.com>
2019-03-14 17:10:17 +0800

    fix orderdict Assignment

--- convert_file.py
+++ convert_file.py
@@ -9,6 +9,7 @@ from nltk.tokenize import TweetTokenizer
 import os
 import jnius_config
 import logging
+import re
 from collections import OrderedDict, defaultdict
 _resources_dir = "./resources"
 jnius_config.add_options('-Xmx512m', '-XX:ParallelGCThreads=2')
@@ -99,6 +100,7 @@ for j, sentence in enumerate(data):
         tweet = tweet[1:-1]
     # tweet_li = tknzr.tokenize(tweet)
     tweet_li = tweet.split()
+    # tweet_li = [re.sub(r"@[\d\w_]*", "@URL", x) for x in tweet_li]
     pos_li = pos_tagger.tagger(tweet_li)
     ### process the tag
     tag_li = []
@@ -135,31 +137,34 @@ for sentence_dict in data_dict.values():
     p_d["\t".join(word_li)]["pos"] = sentence_dict["pos"]
 
 idx = 0
+data_dict_new = defaultdict(lambda : defaultdict(list))
+sentence_dict = defaultdict(list)
 for k, v in p_d.items():
     t = v["tag"]
     p = v["pos"]
+    tag_li = []
     if len(t) > 1: # there are more than 1 NEs in this tweet
         x = ""
-        print(t)
         for i in range(len(t[0])):
-            if t[0][i] != "O": x = t[0][i]
+            if t[0][i] != "O": 
+                x = t[0][i]
             elif t[1][i] != "O": x = t[1][i]
-            else: t = "O"
-            tag_li.append(t)
-    else: tag_li = t
-    sentence_dict["word"] = k.split("\t")
-    sentence_dict["tag"] = tag_li
-    sentence_dict["pos"] = p
-    print(sentence_dict["word"])
-    print(sentence_dict["pos"])
-    data_dict[idx] = sentence_dict
+            else: x = "O"
+            tag_li.append(x)
+    else: tag_li = t[0]
+    data_dict_new[idx]["word"] = k.split("\t")
+    data_dict_new[idx]["tag"] = tag_li
+    data_dict_new[idx]["pos"] = p
+    # data_dict_new[idx] = sentence_dict
     idx += 1
+
+# print(data_dict_new)
 
 print("####there are {} missing ne".format(miss))
 ### convert data_dict to 3 columns
 with open("../data/converted_file_TrainData2.csv", "w") as outputf:
     outputf.write("Sentence #,Word,POS,Tag\n")
-    for idx, sentence_dict in data_dict.items():
+    for idx, sentence_dict in data_dict_new.items():
         word_li = sentence_dict["word"]
         tag_li = sentence_dict["tag"]
         pos_li = sentence_dict["pos"]
