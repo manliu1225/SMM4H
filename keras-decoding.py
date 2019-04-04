796844dfe18181d9c2f8674982e158d522088971
manliu1225 <liumanlalala@gmail.com>
2019-04-04 16:02:24 +0800

    add dimension to aux input

--- keras-decoding.py
+++ keras-decoding.py
@@ -47,6 +47,9 @@ X_test_pos = np.load('../encoded/X_test_pos.npy')
 y_test_ner = np.load('../encoded/y_test_ner.npy')
 X_test_features = np.load('../encoded/X_test_features.npy')
 X_test_sents_bert = np.load('../encoded/X_test_sents_bert.npy')
+
+X_test_features =  np.expand_dims(X_test_features, axis=2)
+
 print("...data loaded!")
 
 # ## load weights
@@ -131,12 +134,11 @@ plt.show()
 
 
 decoded = []
-for sent_idx in range(len(X_test_sents)):
-    
+for sent_idx in range(len(X_test_sents[:500])):
     this_txt = sequence.pad_sequences([X_test_sents[sent_idx]], maxlen=MAX_LENGTH, truncating='post', padding='post')
     this_pos = sequence.pad_sequences([X_test_pos[sent_idx]], maxlen=MAX_LENGTH, truncating='post', padding='post')
     this_feature = sequence.pad_sequences([X_test_features[sent_idx]], maxlen=MAX_LENGTH, truncating='post', padding='post')
-    this_pred = model.predict([this_txt, this_tex, this_pos, this_feature])
+    this_pred = model.predict([this_txt, this_txt, this_pos, this_feature])
     this_pred = [np.argmax(p) for p in this_pred[0]]
     np.shape(this_pred)
 
